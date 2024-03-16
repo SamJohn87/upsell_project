65f5be11f7ead1a579e4b4c0a8671bf71790579c
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2024-03-16 16:19:29 -0500

    created models for cars with class car and car_class

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Car_Class(models.Model):
+    name = models.TextField()
+    order = models.IntegerField()
+    rate = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    model = models.TextField()
+    brand = models.TextField()
+    stall = models.IntegerField()
+    doors = models.IntegerField()
+    seats = models.IntegerField()
+    fuel = models.TextField()
+    android_carplay = models.BooleanField(default=False)
+    touchscreen = models.BooleanField(default=False)
+    backup_camera = models.BooleanField(default=False)
+    awd = models.BooleanField(default=False)
+    high_ground_clearance = models.BooleanField(default=False)
+    built_in_navigation = models.BooleanField(default=False)
+    keyless_entry = models.BooleanField(default=False)
+    leather_seats = models.BooleanField(default=False)
+    memory_seat = models.BooleanField(default=False)
+    collision_warning = models.BooleanField(default=False)
+    lane_keep_assist = models.BooleanField(default=False)
+    blind_spot_monitoring = models.BooleanField(default=False)
+    bluetooth = models.BooleanField(default=False)
+    sunroof = models.BooleanField(default=False)
+    usb_port = models.BooleanField(default=False)
+    car_class = models.ForeignKey(Car_Class, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} Features: Doors: {self.doors}, Seats: {self.seats}, Fuel: {self.fuel}"
